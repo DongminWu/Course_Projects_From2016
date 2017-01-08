b49b2e42b15edf3feddec6d2a6ba6f53bc1c66fa
pipicold <wdm228@126.com>
2017-01-08 21:44:38 +0100

    beautify

--- WS2016-2017_TUBerlin/compiler_design/exercise_P3_LLVM/lab_3/pass.cpp
+++ WS2016-2017_TUBerlin/compiler_design/exercise_P3_LLVM/lab_3/pass.cpp
@@ -22,51 +22,51 @@ using namespace llvm;
 
 namespace {
 
-class DefinitionPass : public FunctionPass {
-public:
-  static char ID;
-  DefinitionPass() : FunctionPass(ID) {}
+    class DefinitionPass : public FunctionPass {
+	public:
+	    static char ID;
+	    DefinitionPass() : FunctionPass(ID) {}
 
-  virtual void getAnalysisUsage(AnalysisUsage &au) const {
-    au.setPreservesAll();
-  }
+	    virtual void getAnalysisUsage(AnalysisUsage &au) const {
+		au.setPreservesAll();
+	    }
 
-  virtual bool runOnFunction(Function &F) {
-    // Example: Print all stores and where they occur
-    for (BasicBlock &BB : F) {
-      for (Instruction &I : BB) {
-        if (StoreInst *SI = dyn_cast<StoreInst>(&I)) {
-          Value *PtrOp = SI->getPointerOperand(); // Store target
-          if (PtrOp->hasName()) {
-            DebugLoc Loc = SI->getDebugLoc();
-            errs() << "Variable " << PtrOp->getName()
-                   << " written on line " << Loc.getLine() << "\n";
-          }
-        }
-      }
-    }
+	    virtual bool runOnFunction(Function &F) {
+		// Example: Print all stores and where they occur
+		for (BasicBlock &BB : F) {
+		    for (Instruction &I : BB) {
+			if (StoreInst *SI = dyn_cast<StoreInst>(&I)) {
+			    Value *PtrOp = SI->getPointerOperand(); // Store target
+			    if (PtrOp->hasName()) {
+				DebugLoc Loc = SI->getDebugLoc();
+				errs() << "Variable " << PtrOp->getName()
+				    << " written on line " << Loc.getLine() << "\n";
+			    }
+			}
+		    }
+		}
 
-    // We did not modify the function
-    return false;
-  }
-};
+		// We did not modify the function
+		return false;
+	    }
+    };
 
-class FixingPass : public FunctionPass {
-public:
-  static char ID;
-  FixingPass() : FunctionPass(ID) {}
+    class FixingPass : public FunctionPass {
+	public:
+	    static char ID;
+	    FixingPass() : FunctionPass(ID) {}
 
-  virtual void getAnalysisUsage(AnalysisUsage &au) const {
-    au.setPreservesCFG();
-  }
+	    virtual void getAnalysisUsage(AnalysisUsage &au) const {
+		au.setPreservesCFG();
+	    }
 
-  virtual bool runOnFunction(Function &F) {
-    // TODO
+	    virtual bool runOnFunction(Function &F) {
+		// TODO
 
-    // The function was modified
-    return true;
-  }
-};
+		// The function was modified
+		return true;
+	    }
+    };
 
 } // namespace
 
